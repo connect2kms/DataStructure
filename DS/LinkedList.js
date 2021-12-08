45e60a13743df4ee0413a8b32f6407fbc51ad02f
Karthik Mohanasundaram <connect2kms@outlook.com>
2021-12-07 20:34:39 -0500

    Linkedlist Push method implemented

---
+++ DS/LinkedList.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+const linkedList = new LinkedList(5);
+linkedList.push(10)
+
+console.log(linkedList);
